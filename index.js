598e511c7c3571839671a3cbab3332b430ed7ea6
Christian Areas <me@areas.me>
2022-12-08 12:21:24 -0800

    Added a new html object to store HTML as it’s generated.

--- index.js
+++ index.js
@@ -22,13 +22,6 @@
 //     THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 // [ ] WHEN I decide to finish building my team
 //     THEN I exit the application, and the HTML is generated
-//
-// Todos:
-// [x] Add unit tests.
-// [x] Add classes and sub-classes.
-// [x] Add HTML template.
-// [ ] Add code to use HTML template to generate final HTML file.
-// [x] Refactor the code so there‘s only one prompt (not counting the continue-or-finish prompt) and "manager", "developer", or "intern" is passed as a variable.
 
 // Import the Inquirer and File System modules.
 const inquire = require("inquirer")
@@ -39,7 +32,7 @@ const Manager = require("./lib/Employee/Manager")
 const Developer = require("./lib/Employee/Developer")
 const Intern = require("./lib/Employee/Intern")
 
-// Generate prompts for an employee role.
+// Generate prompts specific to a given role.
 function generatePrompts(role) {
   let uniquePrompt
   let uniquePromptName
@@ -92,26 +85,6 @@ function generatePrompts(role) {
   return prompts
 }
 
-// Start the app and prompt the user for the manager’s information.
-function start() {
-  inquire
-  .prompt(generatePrompts("Manager"))
-  .then((answers) => {
-    // Use the Manager subclass to create a manager object.
-    const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
-    // Read the employee.html template file and replace all placeholders with the user’s answers and appropriate values.
-    let managerHTML = fs.readFileSync("./src/employee.html", "utf8")
-    for (const [key, value] of Object.entries(manager)) { managerHTML = managerHTML.replaceAll(`{${key}}`, value) }
-    managerHTML = managerHTML.replace("{role}", manager.getRole())
-    managerHTML = managerHTML.replace("{icon}", manager.getIcon())
-    managerHTML = managerHTML.replace("{unique-answer-label}", manager.getOfficeLabel())
-    managerHTML = managerHTML.replace("{unique-answer}", manager.getOffice())
-    // Print a success message and pass the manager role and html to the function that prompts the user to continue or finish.
-    console.log("Great! On to the next step:")
-    promptToContinueOrFinish(manager.getRole(), managerHTML)
-  })
-}
-
 // Declare the continue or finish prompt.
 const continueOrFinishPrompt = [
   { message: "Choose a role to add to the team. Or if you’re done, choose “Finish.”",
@@ -121,31 +94,41 @@ const continueOrFinishPrompt = [
   },
 ]
 
-// Prompt the user to continue or finish.
-function promptToContinueOrFinish(role, html) {
-  inquire
-  .prompt(continueOrFinishPrompt)
-  .then((answer) => {
-    if (answer.role === "Developer") {
-      promptForDeveloperInformation()
-    } else if (answer.role === "Intern") {
-      promptForInternInformation()
-    } else if (answer.role === "Finish") {
-      // Read the index.html template file, replace the placeholder with the html, and write the new index.html file to the dist folder.
-      let finalHTML = fs.readFileSync("./src/index.html", "utf8")
-      finalHTML = finalHTML.replace("{manager}", html)  
-      fs.writeFileSync("./dist/index.html", finalHTML)
-      // Print success message.
-      console.log("Done!") // **x
-    }
+// Declare an object that stores HTML for the final index.html file.
+const html = {
+  managersHTML: [],
+  developersHTML: [],
+  internsHTML: [],
+}
+
+// Start the app and prompt the user for the manager’s information.
+function start() {
+  inquire.prompt(generatePrompts("Manager"))
+  .then((answers) => {
+    // Use the Manager subclass to create a manager object.
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.office)
+    // Read the employee.html template file.
+    let managerHTML = fs.readFileSync("./src/employee.html", "utf8")
+    // Replace all placeholders with the user’s answers and appropriate values.
+    for (const [key, value] of Object.entries(manager)) { managerHTML = managerHTML.replaceAll(`{${key}}`, value) }
+    managerHTML = managerHTML.replace("{role}", manager.getRole())
+    managerHTML = managerHTML.replace("{icon}", manager.getIcon())
+    managerHTML = managerHTML.replace("{unique-answer-label}", manager.getOfficeLabel())
+    managerHTML = managerHTML.replace("{unique-answer}", manager.getOffice())
+    // Push the manager HTML to the html object.
+    html.managersHTML.push(managerHTML)
+    // Print a success message.
+    console.log("Great! On to the next step:")
+    // Pass the updated html object to the function that prompts the user to continue or finish.
+    promptToContinueOrFinish(html)
   })
 }
 
 // Prompt the user for the developer’s information.
 function promptForDeveloperInformation() {
-  inquire
-  .prompt(generatePrompts("Developer"))
+  inquire.prompt(generatePrompts("Developer"))
   .then((answers) => {
+    // Use the Developer subclass to create a developer object.
     const developer = new Developer(answers.name, answers.id, answers.email, answers.github)
     console.log(developer) // **
     promptToContinueOrFinish()
@@ -154,12 +137,33 @@ function promptForDeveloperInformation() {
 
 // Prompt the user for the intern’s information.
 function promptForInternInformation() {
-  inquire
-  .prompt(generatePrompts("Intern"))
+  inquire.prompt(generatePrompts("Intern"))
   .then((answers) => {
+    // Use the Intern subclass to create a intern object.
     const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
     console.log(intern) // **
     promptToContinueOrFinish()
+  })
+}
+
+// Prompt the user to continue or finish.
+function promptToContinueOrFinish(html) {
+  inquire.prompt(continueOrFinishPrompt)
+  .then((answer) => {
+    if (answer.role === "Developer") {
+      promptForDeveloperInformation()
+    } else if (answer.role === "Intern") {
+      promptForInternInformation()
+    } else if (answer.role === "Finish") {
+      // Read the index.html template file.
+      let finalHTML = fs.readFileSync("./src/index.html", "utf8")
+      // Replace all placeholders with the HTML in the html object.
+      finalHTML = finalHTML.replace("{manager}", html.managerHTML[0])
+      // Write the new index.html file to the dist folder.
+      fs.writeFileSync("./dist/index.html", finalHTML)
+      // Print a success message.
+      console.log("Done!") // **
+    }
   })
 }
 
